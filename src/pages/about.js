9c51a525352f4ec0aed25faa01a7f10aa9ba3779
vikramWM <154304021+vikramWM@users.noreply.github.com>
2024-07-20 17:54:50 +0530

    about and contact

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,10 @@ import React from 'react';
 const About = () => {
     return (
         <>
-            <div className="breadcumb-wrapper" style={{
-                backgroundImage: `url("assets/img/bg/breadcumb-bg.jpg")`,
-                backgroundPosition: 'center'
-            }}>
+            {/* <div className="breadcumb-wrapper" style={{
+                    backgroundImage: `url("assets/img/bg/breadcumb-bg.jpg")`,
+                    backgroundPosition: 'center'
+                }}>
                 <div className="container">
                     <div className="breadcumb-content">
                         <h1 className="breadcumb-title">About Us</h1>
@@ -16,7 +16,137 @@ const About = () => {
                         </ul>
                     </div>
                 </div>
+            </div> */}
+            {/* <section class="overflow-hidden bg-top-center th-radius3 m-4 mt-0 mb-0 space" id="testi-sec" style={{ backgroundImage: `url('assets/img/bg/testimonial_bg_5.jpg')` }} data-bg-src="">
+                <div class="container th-container4">
+                    <div class="row justify-content-lg-between justify-content-center align-items-end">
+                        <div class="col-xxl-4 col-xl-6">
+                            <div class="title-area text-center text-lg-start">
+                                <span class="sub-title style1 text-white">
+                                    Customer Feedbacks
+                                </span>
+                                <h2 class="sec-title text-white">See what our clients have to say</h2>
+                            </div>
+                        </div>
+                        <div class="col-lg-auto d-none d-xl-block">
+                            <div class="sec-btn">
+                                <div class="icon-box">
+                                    <button data-slider-prev="#testiSlider5" class="slider-arrow style2 default">
+                                        <i class="far fa-arrow-left"></i>
+                                    </button> 
+                                    <button data-slider-next="#testiSlider5" class="slider-arrow style2 default">
+                                        <i class="far fa-arrow-right"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slider-area"></div>
+                </div>
+            
+            </section> */}
+            <div style={{ height: '137px', backgroundColor: '#18336c'}}></div>
+            {/* <div className="breadcumb-wrapper" style={{
+                    backgroundImage: `url("assets/img/bg/bg.jpg")`,
+                    backgroundPosition: 'center'
+                }}>
+                <div className="container">
+                    <div className="" style={{ textAlign: ''}}>
+                        <h1 className="breadcumb-title">About Us</h1>
+                        <ul className="breadcumb-menu">
+                            <li><a href="/">Home</a></li>
+                            <li>About Us</li>
+                        </ul>
+                    </div>
+                </div>
+            </div> */}
+            <div class="th-hero-wrapper hero-1" id="hero">
+                <div class="hero-img tilt-active">
+                    <img src="assets/img/hero/hero_img_1_1.png" alt="Hero Image" />
+                </div>
+                <div class="container">
+                    <div class="hero-style1">
+                        <span class="hero-subtitle">About</span>
+                        <h1 class="hero-title">Warrgyiz Morsch</h1>
+                        <p class="hero-text">
+                            Welcome to Warrgyizmorsch , your number one source for all the outsourcing services. 
+                            We're dedicated to providing you the very best of technical and non-technical services, 
+                            with an emphasis on meeting the deadline, satisfactory service and 24*7 services. <br />
+                            Founded in 2017, Warrgyizmorsch has come its way from its beginnings. When we first 
+                            started out, our passion for our clients ["to keep you first] drove our clients to 
+                            flourish their business. <br />
+                            We hope you enjoy our services as much as we enjoy offering them to you. 
+                            If you have any questions or comments, please don't hesitate to contact us.
+                        </p>
+                        <div class="btn-group">
+                            <a href="/contact" class="th-btn">
+                                CONTACT US
+                                <i class="fa-regular fa-arrow-right ms-2"></i>
+                            </a>
+                            <div class="call-btn">
+                                <a href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" class="play-btn popup-video">
+                                    <i class="fas fa-play"></i>
+                                </a>
+                                <div class="media-body">
+                                    <a href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" class="btn-title popup-video">
+                                        Watch Our Story
+                                    </a> 
+                                    <span class="btn-text">Subscribe Now</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="hero-shape1"><img src="assets/img/hero/hero_shape_1_1.svg" alt="shape" /></div>
+                <div class="hero-shape2"><img src="assets/img/hero/hero_shape_1_2.svg" alt="shape" /></div>
+                <div class="hero-shape3"><img src="assets/img/hero/hero_shape_1_3.svg" alt="shape" /></div>
             </div>
+            <section class="about-sec-v4 space-bottom">
+                <div class="container">
+                    <div class="row gy-4 justify-content-center">
+                        <div class="col-xl-4 col-md-6">
+                            <div class="feature-card h-100">
+                                <div class="shape-icon">
+                                    <img src="assets/img/icon/feature_card_1.png" alt="icon" />
+                                </div>
+                                <h3 class="box-title">Our Mission</h3>
+                                <p class="feature-card_text">
+                                    Help our clients to grow their businesses by providing cost-effective and world-class 
+                                    best business solutions in London, process outsourcing through a collaborative effort 
+                                    between employees, suppliers and partners. our work. Especially. The quality of the 
+                                    work is not only limited to capacity but also exceeds the expectations of customers.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-6">
+                            <div class="feature-card h-100">
+                                <div class="shape-icon">
+                                    <img src="assets/img/icon/feature_card_2.png" alt="icon" />
+                                </div>
+                                <h3 class="box-title">Our Vision</h3>
+                                <p class="feature-card_text">
+                                    Our vision is to be remained a world-class, diversified and socially responsible business 
+                                    development company that provides high quality, value-added services to our clients.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-6">
+                            <div class="feature-card h-100">
+                                <div class="shape-icon">
+                                    <img src="assets/img/icon/feature_card_3.png" alt="icon" />
+                                </div>
+                                <h3 class="box-title">Our Value</h3>
+                                <p class="feature-card_text">
+                                    We believe in transparency at work & our clients. We ensure hassle free delivery within 
+                                    the stipulated time frame. Our teams work for your business with a project manager's 
+                                    supervision. We are committed to delivering high-quality services. We work tirelessly 
+                                    for our clients to ensure they earn abounding profits.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,156 @@
 import React from 'react'
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <>
+            <div style={{ height: '137px', backgroundColor: '#18336c'}}></div>
+            {/* <div class="space">
+                <div class="container">
+                    <div class="row gy-4">
+                        <div class="col-xl-6 col-md-6">
+                            <div class="contact-info">
+                                <div class="contact-info_icon">
+                                    <i class="fas fa-location-dot"></i>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="box-title">Our Office Address</h4>
+                                    <span class="contact-info_text">International House, Constance Street, <br /> London E16</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-md-6">
+                            <div class="contact-info">
+                                <div class="contact-info_icon">
+                                    <i class="fas fa-location-dot"></i>
+                                </div>                                        
+                                <div class="media-body">
+                                    <h4 class="box-title">Our India Branch</h4>
+                                    <span class="contact-info_text">Warrgyizmorsch private limited <br /> 27 krishna vihar Behind mds school old rto <br />Pratap nagar Udaipur, Rajasthan</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-md-6">
+                            <div class="contact-info">
+                                <div class="contact-info_icon"><i class="fas fa-phone"></i></div>
+                                <div class="media-body">
+                                    <h4 class="box-title">Call Us Anytime</h4>
+                                    <span class="contact-info_text">
+                                        <a href="tel:+442030533844">+44 2030533844</a> 
+                                        <a href="tel:+919784710045">+91 9784710045</a>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-md-6">
+                            <div class="contact-info">
+                                <div class="contact-info_icon">
+                                    <i class="fas fa-envelope"></i>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="box-title">Send An Email</h4>
+                                    <span class="contact-info_text">
+                                        <a href="mailto:info@warrgyizmorsch.com">info@warrgyizmorsch.com</a>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+            <section class="about-sec-v4 pb-5">
+                <div class="container ">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 mb-30 mb-lg-0">
+                            <div class="img-box6">
+                                <div class="img1"><img src="assets/img/normal/about_4_1.png" alt="About" /></div>
+                                <div class="shape1"><img src="assets/img/normal/about_4_2.png" alt="About" /></div>
+                                <div class="shape2"><img src="assets/img/normal/about_4_3.png" alt="About" /></div>
+                                <div class="color-animate"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-8 text-lg-start text-center">
+                            <div class="ps-xxl-5">
+                                <div class="title-area mb-35">
+                                    <span class="sub-title">Contact Info</span>
+                                    <h2 class="sec-title">Warrgyizmorsch private limited</h2>
+                                </div>
+                                <div class="checklist style4 mb-20 list-center">
+                                    <ul>
+                                        <li>
+                                            <i class="fas fa-location-dot"></i>
+                                            <b>Our Office Address :</b> International House, Constance Street, London E16                                                
+                                        </li>
+                                        <li>
+                                            <i class="fas fa-location-dot"></i>
+                                            <b>Our India Branch :</b> 27 krishna vihar Behind mds school old rto Pratap nagar Udaipur, Rajasthan                                       
+                                        </li>
+                                        <li>
+                                            <i class="fas fa-envelope"></i>
+                                            <b>Send An Email :</b><a href="mailto:info@warrgyizmorsch.com">info@warrgyizmorsch.com</a>                                                
+                                        </li>
+                                        <li>
+                                            <i class="fas fa-phone"></i>                                            
+                                            <b>Call Us Anytime:</b>
+                                            <a href="tel:+442030533844">+44 2030533844</a> 
+                                            <a href="tel:+919784710045">+91 9784710045</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div class="bg-smoke space" style={{ backgroundImage: `url('assets/img/bg/contact_bg_1.png')` }}data-bg-src="">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-8">
+                            <div class="title-area mb-35 text-xl-start text-center">
+                                <span class="sub-title">
+                                    contact with us!
+                                </span>
+                                <h2 class="sec-title">Have Any Questions?</h2>
+                                {/* <p class="sec-text">
+                                Tell us about yourself
+                                </p> */}
+                            </div>
+                            <form action="https://html.themeholy.com/webteck/demo/mail.php" method="POST" class="contact-form ajax-contact">
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <input type="text" class="form-control" name="name" id="name" placeholder="Your Name" /> 
+                                        <i class="fal fa-user"></i>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" /> 
+                                        <i class="fal fa-envelope"></i>
+                                    </div>                                    
+                                    <div class="form-group col-md-6">
+                                        <input type="tel" class="form-control" name="number" id="number" placeholder="Phone Number" /> 
+                                        <i class="fal fa-phone"></i>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <input type="text" class="form-control" name="CompanyName" id="CompanyName" placeholder="Company Name" /> 
+                                        <i class="fal fa-building"></i>
+                                    </div>
+                                    <div class="form-group col-12">
+                                        <textarea name="message" id="message" cols="30" rows="3" class="form-control" placeholder="Your Message"></textarea> 
+                                        <i class="fal fa-comment"></i>
+                                    </div>
+                                    <div class="form-btn text-xl-start text-center col-12">
+                                        <button class="th-btn">
+                                            Send Message
+                                            <i class="fa-regular fa-arrow-right ms-2"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <p class="form-messages mb-0 mt-3"></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Contact;
